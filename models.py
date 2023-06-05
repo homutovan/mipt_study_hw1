aa87d248b62e6631207eb142cd00f2f61dab422f
Andrey Khomutov <homutovan@live.com>
2023-06-05 12:12:12 +0700

    add Company model

--- models.py
+++ models.py
@@ -1,15 +1,28 @@
-from sqlalchemy import Column, BigInteger, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, BigInteger, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
 class TypeOfBusiness(Base):
     __tablename__ = 'type_of_buisness'
 
-    id = Column(BigInteger, primary_key=True, autoincrement=True)
-    code = Column(String(255), nullable=False, index=True)
+    id = Column(BigInteger, autoincrement=True, primary_key=True)
+    code = Column(String(255), nullable=False, unique=True)
     parent_code = Column(String(255), nullable=False)
     section = Column(String(255), nullable=False)
     name = Column(String(1024), nullable=False)
     comment = Column(String)
+    companies = relationship('Company')
+
+
+class Company(Base):
+    __tablename__ = 'company'
+    id = Column(BigInteger, autoincrement=True, primary_key=True)
+    name = Column(String(1024), nullable=False)
+    full_name = Column(String(1024), nullable=False)
+    inn = Column(String(255), nullable=False)
+    kpp = Column(String(255), nullable=False)
+    # okved = Column(String(255), nullable=False)
+    okved = Column(String(255), ForeignKey('type_of_buisness.code'))
+
 
